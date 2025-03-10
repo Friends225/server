6d89274f4fdb7944df2601cd4f641cce96f35795
Hamza-Tasadaq <hamzatasadaq01@gmail.com>
2025-03-10 15:12:54 +0500

    file removed

--- DB/SupabaseClient.js
+++
@@ -1,8 +0,0 @@
-
-const {createClient}=require('@supabase/supabase-js')
-require('dotenv').config()
-const supabaseUrl = process.env.SUPABASE_APP_URL;
-const supabaseAnonKey = process.env.SUPABASE_APP_ANON_KEY;
-
- const supabase = createClient(supabaseUrl, supabaseAnonKey);
- module.exports={supabase}
